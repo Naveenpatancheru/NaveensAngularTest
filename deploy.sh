5e2883452f949a9e703c28e5e9ffcb586ccdd29b
NaveenPatancheru <patan.naveen@gmail.com>
2018-12-13 15:39:05 -0500

    Checking

--- deploy.sh
+++ deploy.sh
@@ -114,7 +114,7 @@ if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
   cd - > /dev/null
 fi
 # 3. Angular Prod Build
-IF EXIST "$DEPLOYMENT_SOURCE/angular.json" (
+IF EXIST "$DEPLOYMENT_SOURCE/angular.json"; (
 echo Building App in $DEPLOYMENT_SOURCE…
 pushd "$DEPLOYMENT_SOURCE"
 call :ExecuteCmd !NPM_CMD! run build
